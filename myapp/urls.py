54dc8065dc4a1b208a7f90c72807c60aa339fb20
khaoula-abaidi <abaidik@gmail.com>
2019-12-17 00:27:36 +0100

    crud without bootstrap

---
+++ myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('',views.index,name="show"),
+    path('update/<str:pk>',views.updateTask,name='update_task'),
+    path('delete/<str:pk>',views.deleteTask,name='delete_task'),
+]
